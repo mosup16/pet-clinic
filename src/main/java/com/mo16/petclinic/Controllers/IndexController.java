572038a04b478b1538ccd7e4b749b492b39766ff
mosup16 <mosup16@gmail.com>
2020-01-03 17:32:01 +0200

    first git commit

--- src/main/java/com/mo16/petclinic/Controllers/IndexController.java
+++ src/main/java/com/mo16/petclinic/Controllers/IndexController.java
@@ -8,6 +8,6 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index", "index.html"})
     public String redirectHome() {
-        return "index";
+        return "welcome";
     }
 }
